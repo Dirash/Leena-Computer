6b34e6037a95f3f8fdcc1086e897216a8e756098
dirash <dirashche10@gmail.com>
2024-10-06 22:38:49 +0530

    first commit

---
+++ src/main/java/Dirash_Company/Ecommerce_Framework/App.java
@@ -0,0 +1,10 @@
+package Dirash_Company.Ecommerce_Framework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
